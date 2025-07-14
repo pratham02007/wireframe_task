52943b3ff6a38b620978efe22b8b2dcf008f844d
pratham02007 <pratham02007@gmail.com>
2025-07-14 11:03:09 +0530

    Add files via upload

--- homework.py
+++ homework.py
@@ -6,3 +6,8 @@ st.title("My First Streamlit APP")
 
 st.write("Welcome to my app! This is my simple wireframe.")
 st.subheader("Student Information")
+text = st.text_input("Enter your name:")
+st.write(f"Your name is {text}")
+age = [1, 2, 3, 4, 5, 6,7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,] 
+select_age = st.selectbox("Select your age:", age )
+st.write(f"Your age is : {select_age}")
