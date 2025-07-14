672c04bfe6b7c87ce915a64495b6f6cdb5c5db6c
pratham02007 <pratham02007@gmail.com>
2025-07-14 10:37:35 +0530

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title("My First Streamlit APP")
+
+st.write("Welcome to my app! This is my simple wireframe.")
+st.subheader("Student Information")
